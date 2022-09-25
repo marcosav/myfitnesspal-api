6ee9cd9ee24cb4c175ae5209d1f662c00ff327c2
marcosav <marcosav2010@gmail.com>
2022-09-25 16:15:30 +0200

    Increased login exception visibility

--- src/main/java/com/gmail/marcosav2010/myfitnesspal/api/LoginException.java
+++ src/main/java/com/gmail/marcosav2010/myfitnesspal/api/LoginException.java
@@ -2,11 +2,11 @@ package com.gmail.marcosav2010.myfitnesspal.api;
 
 public class LoginException extends Exception {
 
-    LoginException(String message, Throwable cause) {
+    public LoginException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    LoginException(String message) {
+    public LoginException(String message) {
         super(message);
     }
 }
